de33e662d97686440917418a73a2d12559478af9
Weifan Wu <weifan.wu@ceres.scinet.usda.gov>
2025-03-11 21:46:35 -0500

    update

--- makeblastdb.sh
+++ makeblastdb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-GENOME_DIR="ncbi_genomes"
-BLAST_DB_DIR="ncbi_blast_db"
+GENOME_DIR="database/EB_complete_genomes"
+BLAST_DB_DIR="database/EB_blast_db"
+mkdir -p "${BLAST_DB_DIR}"
 EB_GENUS=("Escherichia" "Salmonella" "Shigella" "Klebsiella" "Enterobacter" "Cronobacter" "Citrobacter")
 create_blastdb() {
 local INPUT_FASTA="$1"
@@ -13,22 +14,22 @@ echo "BLAST database created for $DB_NAME at ${BLAST_DB_DIR}/${DB_NAME}"
 for GENUS in "${EB_GENUS[@]}"; do
 echo "Processing genus: $GENUS"
 GENUS_FASTA="${GENOME_DIR}/${GENUS}/${GENUS}_all_genomes.fna"
-cat "${GENOME_DIR}/${GENUS}"/*/*_genomic.fna > "$GENUS_FASTA"
+find "${GENOME_DIR}/${GENUS}" -type f -name "*_genomic.fna" -exec cat {} + > "$GENUS_FASTA"
 create_blastdb "$GENUS_FASTA" "$GENUS"
 # Species-level databases
 SPECIES_LIST=$(find "${GENOME_DIR}/${GENUS}" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
 for SPECIES in $SPECIES_LIST; do
 SPECIES_FASTA="${GENOME_DIR}/${GENUS}/${SPECIES}/${SPECIES}_all_genomes.fna"
-cat "${GENOME_DIR}/${GENUS}/${SPECIES}"/*_genomic.fna > "$SPECIES_FASTA"
+find "${GENOME_DIR}/${GENUS}/${SPECIES}" -type f -name "*_genomic.fna" -exec cat {} + > "$SPECIES_FASTA"
 create_blastdb "$SPECIES_FASTA" "${SPECIES}"
 done
 done
-echo "Processing serotype level database for Salmonella"
+echo "Processing serotype level database for Salmonella enterica"
 SEROTYPE_DIR="${GENOME_DIR}/Salmonella/Salmonella_enterica"
 for DIR in "${SEROTYPE_DIR}"/*; do
 SEROTYPE=$(basename "$DIR")
-SEROTYPE_FASTA="${SEROTYPE_DIR}/${SEROTYPE}_genomes.fna"
-cat "${DIR}"/*_genomic.fna > "$SEROTYPE_FASTA"
-create_blastdb "$SEROTYPE_FASTA" "Salmonella_enterica_${SEROTYPE}"
+SEROTYPE_FASTA="${DIR}/${SEROTYPE}_genomes.fna"
+find "${DIR}" -type f -name "*_genomic.fna" -exec cat {} + > "$SEROTYPE_FASTA"
+create_blastdb "$SEROTYPE_FASTA" "Salmonella_${SEROTYPE}"
 done
 
