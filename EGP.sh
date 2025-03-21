9bc12a8fcd8585e9218a91bcba16549a1459416f
Weifan Wu <weifan.wu@ceres.scinet.usda.gov>
2025-03-21 00:32:22 -0500

    update

--- EGP.sh
+++ EGP.sh
@@ -7,7 +7,7 @@ WORK_DIR=$(pwd)
 TAXON_FILE=""
 GENE_FILE=""
 OUTPUT_FILE="${WORK_DIR}/result/gene_summary.csv"
-EB_DRAFT_GENOMES_DIR="${WORK_DIR}/database/draft_genomes"
+DRAFT_GENOMES_DIR="${WORK_DIR}/database/draft_genomes"
 BLAST_DB_DIR="${WORK_DIR}/database/complete_blast_db"
 BLAST_RESULT_DIR="${WORK_DIR}/result/complete_blast_results"
 FILTERED_BLAST_RESULT_DIR="${WORK_DIR}/result/filtered_complete_blast_results"
@@ -22,7 +22,7 @@ echo "-g GENE_FILE : FASTA file with target gene sequence (required)."
 echo "-t TAXON_FILE : File containing target taxons (one target per line) or a single taxon name (e.g., "Salmonella Typhimurium"); A taxon_file is always recommended and must be provided in heavy mode."
 echo "-c COVERAGE : The minimum genome coverage (default: 80%)." 
 echo "-i IDENTITY : The minimum percentage of identity (default: 90%)."
-echo "-o OUTPUT_FILE : Output result file (default: EB_gene_summary.tsv)."
+echo "-o OUTPUT_FILE : Output result file (default: gene_summary.tsv)."
 echo "--mode light|heavy : Run in light mode (complete genomes only) or heavy mode (complete+draft genomes, default: light)."
 echo "--overwrite : If overwrite mode is enabled, previous results will be cleared (default: false)."
 }
@@ -42,7 +42,7 @@ esac
 shift
 done 
 if [[ "$OVERWRITE" == true ]]; then
-rm -rf "$BLAST_RESULT_DIR" "$FILTERED_BLAST_RESULT_DIR" "$DRAFT_BLAST_RESULT_DIR" "$FILTERED_BLAST_RESULT_DIR" "$OUTPUT_FILE"
+rm -rf "$BLAST_RESULT_DIR" "$FILTERED_BLAST_RESULT_DIR" "$DRAFT_BLAST_RESULT_DIR" "$DRAFT_BLAST_DB_DIR" "$FILTERED_BLAST_RESULT_DIR" "$DRAFT_GENOMES_DIR" "$FILTERED_DRAFT_BLAST_RESULT_DIR" "$OUTPUT_FILE"
 fi
 # Ensure gene file is provided
 if [[ -z "$GENE_FILE" ]]; then
@@ -60,6 +60,12 @@ mkdir -p "$BLAST_RESULT_DIR" "$FILTERED_BLAST_RESULT_DIR"
 source "${WORK_DIR}/function.sh" || { echo "Error sourcing function.sh". exit 1; }
 # Set delimiter as a space
 if [[ -n "$TAXON_FILE" ]]; then
+if [[ ! -f "$TAXON_FILE" ]]; then
+tmp_taxon_file=$(mktemp)
+echo "$TAXON_FILE" > "$tmp_taxon_file"
+TAXON_FILE="$tmp_taxon_file"
+trap "rm -f '$tmp_taxon_file'" EXIT
+fi
 DELIMITER=" "
 sed 's/[\t,]\+/ /g' "$TAXON_FILE" > "${TAXON_FILE}_processed"
 # Ensure if a taxon file is provided, the genus should be one of the target Enterobacteriaceae
@@ -82,9 +88,10 @@ fi
 process_complete_genomes() {
 local taxon="$1"
 echo "Processing complete genomes for $taxon"
-local blast_db_name="${taxon// /_}"
-blast_db_name="${blast_db_name//./}"
-local blast_output="${BLAST_RESULT_DIR}/${blast_db_name}_complete_blast_results.txt"
+local clean_taxon="$(extract_taxon_info "$taxon")"
+local blast_output_name="${clean_taxon// /_}"
+blast_output_name="${blast_output_name//./}"
+local blast_output="${BLAST_RESULT_DIR}/${blast_output_name}_complete_blast_results.txt"
 if [[ ! -s "$blast_output" ]]; then
 perform_blast "$GENE_FILE" "$MIN_IDENTITY" "$BLAST_RESULT_DIR" "" "$taxon"
 echo "BLAST result saved to $blast_output"
@@ -104,14 +111,17 @@ read -r sample_size iterations <<< "$(calculate_sample_size_and_iterations "$tot
 echo "Processing $taxon | Total draft genomes: $total_draft_genomes. Running $iterations iterations (max 20)."
 for ((i=1; i<=iterations; i++)); do
 echo "Starting iterations $i/$iterations for $taxon"
-download_random_draft_genomes "$taxon" "$sample_size" "$EB_DRAFT_GENOMES_DIR" "$i"
+download_random_draft_genomes "$taxon" "$sample_size" "$DRAFT_GENOMES_DIR" "$i"
 perform_blast "$GENE_FILE" "$MIN_IDENTITY" "$DRAFT_BLAST_RESULT_DIR" "$i" ""
-local blast_db_name="${taxon// /_}"
+local clean_taxon="$(extract_taxon_info "$taxon")"
+local blast_db_name="${clean_taxon// /_}"
 blast_db_name="${blast_db_name//./}"
-local blast_result_file="${DRAFT_BLAST_RESULT_DIR}/${blast_db_name}/iteration${i}_draft_blast_results.txt"
+mkdir -p "${DRAFT_BLAST_RESULT_DIR}"
+local blast_result_file="${DRAFT_BLAST_RESULT_DIR}/${blast_db_name}/iteration_${i}_draft_blast_results.txt"
 filter_blast_results "$blast_result_file" "$FILTERED_DRAFT_BLAST_RESULT_DIR" "$MIN_COVERAGE" "draft"
 done
 }
+
 # Set up the output file headers
 if [[ "$MODE" == "heavy" ]]; then
 echo -e "Organism,Gene_ID,Min_percentage_of_coverage,Min_percentage_of_identity,Total_draft_genomes,Total_complete_genomes,Draft_genomes_sample_size,Number_of_iterations,Complete_genomes_with_target_genes,Draft_genomes_with_target_genes,Percentage_with_target_genes_complete_genomes,Percentage_with_target_genes_draft_genomes" > "$OUTPUT_FILE"
@@ -119,14 +129,7 @@ else
 echo -e "Organism,Gene_ID,Min_percentage_of_coverage,Min_percentage_of_identity,Total_draft_genomes,Total_complete_genomes,Complete_genomes_with_target_genes,Percentage_with_target_genes_complete_genomes" > "$OUTPUT_FILE"
 fi
 # Initialize TAXON_LIST
-TAXON_LIST=""
 if [[ -n "$TAXON_FILE" ]]; then
-if [[ ! -f "$TAXON_FILE" ]]; then
-tmp_taxon_file=$(mktemp)
-echo "$TAXON_FILE" > "$tmp_taxon_file"
-TAXON_FILE="$tmp_taxon_file"
-trap "rm -f '$tmp_taxon_file'" EXIT
-fi
 TAXON_LIST=$(< "$TAXON_FILE")
 else
 # Rmove numeric suffix and convert underscores and dots to spaces
@@ -140,7 +143,8 @@ process_complete_genomes "$taxon"
 [[ "$MODE" == "heavy" ]] && process_draft_genomes "$taxon"
 TOTAL_COMPLETE_GENOMES=$(get_total_genomes_count "$taxon" "complete") 
 TOTAL_DRAFT_GENOMES=$(get_total_genomes_count "$taxon" "contig")
-local_taxon="${taxon// /_}"
+clean_taxon="$(extract_taxon_info "$taxon")"
+local_taxon="${clean_taxon// /_}"
 local_taxon="${local_taxon//./}"
 if [[ "$MODE" == "heavy" && "$TOTAL_DRAFT_GENOMES" -gt 0 ]]; then
 read -r DRAFT_SAMPLE_SIZE ITERATIONS <<< "$(calculate_sample_size_and_iterations "$TOTAL_DRAFT_GENOMES")"
@@ -159,23 +163,26 @@ COMPLETE_GENOMES_WITH_TARGET_GENES=$(awk -v gene="$GENE_ID" '$1 == gene {print $
 if [[ "$MODE" == "heavy" && "$TOTAL_DRAFT_GENOMES" -gt 0 ]]; then
 TOTAL_DRAFT_GENOMES_WITH_TARGET_GENES=0
 for ((i=1; i<="$ITERATIONS"; i++)); do
-DRAFT_GENOMES_WITH_TARGET_GENES=$(awk -v gene="$GENE_ID" '$1 == gene {print $2}' "$FILTERED_DRAFT_BLAST_RESULT_DIR/$local_taxon/filtered_iteration${i}_draft_blast_results.txt" 2>/dev/null | awk -F'_' '{print substr($2, 1, 8)}' | sort -u | wc -l)
+echo "DEBUG: Checking for gene $GENE_ID in draft results:"
+head "$FILTERED_DRAFT_BLAST_RESULT_DIR/${local_taxon}/filtered_iteration_${i}_draft_blast_results.txt"
+DRAFT_GENOMES_WITH_TARGET_GENES=$(awk -v gene="$GENE_ID" '$1 == gene {print $2}' "$FILTERED_DRAFT_BLAST_RESULT_DIR/${local_taxon}/filtered_iteration_${i}_draft_blast_results.txt" 2>/dev/null | cut -c1-8 | sort -u | wc -l)
+echo "$DRAFT_GENOMES_WITH_TARGET_GENES"
 TOTAL_DRAFT_GENOMES_WITH_TARGET_GENES=$((TOTAL_DRAFT_GENOMES_WITH_TARGET_GENES + DRAFT_GENOMES_WITH_TARGET_GENES))
+echo "$TOTAL_DRAFT_GENOMES_WITH_TARGET_GENES"
 done
-AVERAGE_DRAFT_GENOMES_WITH_TARGET_GENES=$(echo "scale=2; $TOTAL_DRAFT_GENOMES_WITH_TARGET_GENES/$ITERATIONS" | bc 2>/dev/null)
+AVERAGE_DRAFT_GENOMES_WITH_TARGET_GENES=$(echo "$TOTAL_DRAFT_GENOMES_WITH_TARGET_GENES/$ITERATIONS" | bc 2>/dev/null)
 PERCENT_WITH_TARGET_GENES_DRAFT_GENOMES=$(echo "scale=2; ($AVERAGE_DRAFT_GENOMES_WITH_TARGET_GENES/$DRAFT_SAMPLE_SIZE) * 100" | bc 2>/dev/null)
 else
 DRAFT_GENOMES_WITH_TARGET_GENES=0
 PERCENT_WITH_TARGET_GENES_DRAFT_GENOMES=0
 fi
-
 if [[ "$TOTAL_COMPLETE_GENOMES" -gt 0 ]]; then
 PERCENT_WITH_TARGET_GENES_COMPLETE_GENOMES=$(echo "scale=2; ($COMPLETE_GENOMES_WITH_TARGET_GENES/$TOTAL_COMPLETE_GENOMES) * 100" | bc 2>/dev/null)
 else
 PERCENT_WITH_TARGET_GENES_COMPLETE_GENOMES=0
 fi
 if [[ "$MODE" == "heavy" ]]; then
-echo -e "$taxon,$GENE_ID,$MIN_COVERAGE,$MIN_IDENTITY,$TOTAL_DRAFT_GENOMES,$TOTAL_COMPLETE_GENOMES,$DRAFT_SAMPLE_SIZE,$ITERATIONS,$COMPLETE_GENOMES_WITH_TARGET_GENES,$DRAFT_GENOMES_WITH_TARGET_GENES,${PERCENT_WITH_TARGET_GENES_COMPLETE_GENOMES}%,${PERCENT_WITH_TARGET_GENES_DRAFT_GENOMES}%" >> "$OUTPUT_FILE"
+echo -e "$taxon,$GENE_ID,$MIN_COVERAGE,$MIN_IDENTITY,$TOTAL_DRAFT_GENOMES,$TOTAL_COMPLETE_GENOMES,$DRAFT_SAMPLE_SIZE,$ITERATIONS,$COMPLETE_GENOMES_WITH_TARGET_GENES,$AVERAGE_DRAFT_GENOMES_WITH_TARGET_GENES,${PERCENT_WITH_TARGET_GENES_COMPLETE_GENOMES}%,${PERCENT_WITH_TARGET_GENES_DRAFT_GENOMES}%" >> "$OUTPUT_FILE"
 else
 echo -e "$taxon,$GENE_ID,$MIN_COVERAGE,$MIN_IDENTITY,$TOTAL_DRAFT_GENOMES,$TOTAL_COMPLETE_GENOMES,$COMPLETE_GENOMES_WITH_TARGET_GENES,${PERCENT_WITH_TARGET_GENES_COMPLETE_GENOMES}%"  >> "$OUTPUT_FILE"
 fi
--- function.sh
+++ function.sh
@@ -78,17 +78,16 @@ mkdir -p "$iteration_dir"
 local accessions=""
 if [[ "$query" == "Salmonella enterica subsp. enterica serovar monophasic Typhimurium" ]]; then
 while read -r actual_name; do
-accessions+=$(ncbi-genome-download bacteria --genera "Salmonella enterica subsp. enterica serovar $actual_name" --assembly-level contig --section genbank --dry-run | tail -n +2 | awk -F '/' '{print $NF}' | shuf -n "$sample_size" | awk '{print $1}')
+accessions+=$(ncbi-genome-download bacteria --genera "Salmonella enterica subsp. enterica serovar $actual_name" --assembly-level contig --section genbank --dry-run | tail -n +2 | awk -F '/' '{print $NF}' | awk '{print $1}')
 done < "$MONOPHASIC_TYPHIMURIUM_LIST"
 else
-accessions=$(ncbi-genome-download bacteria --genera "$query" --assembly-level contig --section genbank --dry-run | tail -n +2 | awk -F '/' '{print $NF}' | shuf -n "$sample_size" | awk '{print $1}')
+accessions=$(ncbi-genome-download bacteria --genera "$query" --assembly-level contig --section genbank --dry-run | tail -n +2 | awk -F '/' '{print $NF}' | awk '{print $1}')
 fi
-echo "$accessions" > "${iteration_dir}/selected_accessions.txt"
-datasets download genome accession --inputfile "${iteration_dir}/selected_accessions.txt" --filename "${iteration_dir}/download.zip"
-unzip -q -o "${iteration_dir}/download.zip" -d "${iteration_dir}"
-find "${iteration_dir}/ncbi_dataset" -type f -name "*_genomic.fna" -exec mv {} "${iteration_dir}" \;
-rm "${iteration_dir}/download.zip"
-rm -rf "${iteration_dir}/ncbi_dataset"
+valid_accessions=$(echo "$accessions" | grep -E '^GCA_[0-9]+\.[0-9]+$' | shuf -n "$sample_size")
+echo "$valid_accessions" > "${iteration_dir}/selected_accessions.txt"
+ncbi-genome-download bacteria --assembly-accessions "$iteration_dir/selected_accessions.txt" --formats fasta --assembly-level contig --section genbank  --output-folder "$iteration_dir"
+find "$iteration_dir/genbank" -type f -name "*_genomic.fna.gz" -exec sh -c 'gzip -d "$0" && mv "${0%.gz}" "'"$iteration_dir"'"' {} \;
+rm -rf "$iteration_dir/genbank"
 } 
 
 function perform_blast(){
@@ -99,13 +98,17 @@ local iteration="$4" # only used for draft genomes
 local taxon_file="$5"  # only provide for complete genomes, always empty for draft genomes
 if [[ -n "$iteration" ]]; then
 echo "Processing draft genomes for iteration $iteration"
-local taxon_dirs=("${EB_DRAFT_GENOMES_DIR}"/*/)
+local taxon_dirs=("${DRAFT_GENOMES_DIR}"/*/)
 local taxon=$(basename "${taxon_dirs[0]%/}")
-local genome_dir="${EB_DRAFT_GENOMES_DIR}/${taxon}/genomes_${iteration}"
+local genome_dir="${DRAFT_GENOMES_DIR}/${taxon}/genomes_${iteration}"
 local blast_db="${DRAFT_BLAST_DB_DIR}/${taxon}/iteration_${iteration}"
 mkdir -p "$(dirname "$blast_db")"
+if [ ! -d "$genome_dir" ] || [ -z "$(find "$genome_dir" -type f -name "*_genomic.fna" 2>/dev/null)" ]; then
+echo "Error: No genomic.fna files found in $genome_dir. Check download process." >&2
+exit 1
+fi
 local concatenated_genome="${genome_dir}/combined.fna"
-cat "$genome_dir"/*_genomic.fna > "$concatenated_genome"
+find "$genome_dir" -name "*_genomic.fna" -exec cat {} + > "$concatenated_genome"
 makeblastdb -in "$concatenated_genome" -dbtype nucl -out "$blast_db"
 local blast_output="${output_dir}/${taxon}/iteration_${iteration}_draft_blast_results.txt"
 mkdir -p "$(dirname "$blast_output")"
@@ -117,7 +120,10 @@ else
 local blast_db_name="${taxon// /_}"
 blast_db_name="${blast_db_name//./}"
 local blast_db="${BLAST_DB_DIR}/${blast_db_name}"
-local blast_output="${output_dir}/${blast_db_name}_complete_blast_results.txt"
+local clean_taxon="$(extract_taxon_info "$taxon")"
+local blast_output_name="${clean_taxon// /_}"
+blast_output_name="${blast_output_name//./}"
+local blast_output="${output_dir}/${blast_output_name}_complete_blast_results.txt"
 blastn -query "$query_gene" -db "$blast_db" -out "$blast_output" -outfmt 6 -perc_identity "$perc_identity" -max_target_seqs 7000
 echo "BLAST results saved to $blast_output"
 fi
@@ -133,8 +139,8 @@ local base_name="$(basename "$blast_result_file")"
 if [[ "$genome_type" == "draft" ]]; then
 local taxon="$(basename "$(dirname "$blast_result_file")")"
 local iteration="$(grep -o '[0-9]\+' <<< "$base_name")"
-local filtered_result="${output_dir}/${taxon}/filtered_iteration${iteration}_draft_blast_results.txt"
-mkdir -p "$(dirname "$filtered_output")"
+local filtered_result="${output_dir}/${taxon}/filtered_iteration_${iteration}_draft_blast_results.txt"
+mkdir -p "$(dirname "$filtered_result")"
 awk -v cov="$coverage_threshold" '(($4/($8 - $7 + 1)*100) >= cov) {print $0}' "$blast_result_file" > "$filtered_result"
 echo "Filtered BLAST results for draft genomes are saved to $filtered_result"
 else
